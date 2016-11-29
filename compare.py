d7c8321a3dc55aa19aa28a740ec635614b01726d
Jay Williams <course@jaywilliams.me>
2016-11-29 19:53:46 +0000

    Squashed that bug

--- compare.py
+++ compare.py
@@ -1,8 +1,8 @@
 import hashlib
 import urllib.request as urllib2
 def compare(remoteHash, localName):
-    data = urllib2.urlopen(target_url) # it's a file like object and works just like a file
+    data = urllib2.urlopen(remoteHash) # it's a file like object and works just like a file
     remoteHash = data.read()
     data.close()
     with open(localName, 'rb') as f: localHash = hashlib.md5(f.read()).hexdigest()
-    print(remoteHash + "\n" + localHash   
+    print(remoteHash + "\n" + localHash)
