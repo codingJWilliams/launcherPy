f6f3e967d2c12c36882b424d52817a7c470569fc
Jay Williams <course@jaywilliams.me>
2016-11-29 20:11:59 +0000

    Added loads, including more verbosity and cleaner code

--- compare.py
+++ compare.py
@@ -1,8 +1,20 @@
 import hashlib
 import urllib.request as urllib2
-def compare(remoteHash, localName):
+langPack = {
+    "grabbingRemote": " Download       | Retrieving remote hash from {}",
+    "removingCrap":   " Calculation    | Removing the rubbish that web sends",
+    "readingLocal":   " IO             | Reading mod into memory from local file, {}",
+    "notice":         " Note           | Remote Hash: {} Local Hash: {}",
+    
+}
+def compare(remoteHash, localName, lang = langPack):
+    print(lang["grabbingRemote"].format(remoteHash))
     data = urllib2.urlopen(remoteHash) # it's a file like object and works just like a file
-    remoteHash = data.read()
+    remoteHashTemp = data.read()
     data.close()
-    with open(localName, 'rb') as f: localHash = hashlib.md5(f.read()).hexdigest()
-    print(remoteHash + "\n" + localHash)
+    print(lang['readingLocal'].format(localName))
+    with open(localName, 'rb') as f:
+        localHash = hashlib.md5(f.read()).hexdigest()
+    print(lang["removingCrap"])
+    remoteHash = remoteHashTemp.decode('utf-8').replace("\n", "")
+    print(lang['notice'].format(remoteHash, localHash))
