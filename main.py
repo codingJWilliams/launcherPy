a745c4ded2f8fc8790725cf84ca8d937414055a4
Jay Williams <course@jaywilliams.me>
2016-11-30 19:04:29 +0000

    Added a slightly dodgy way of getting hashes of mods

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+# Import neccesary libraries
+
 import compare, json
+import urllib.request as urllib2
 
 # Prefs
 
 baseUrl = "http://cdn.damnapi.me/beta"
+baseModFolder = "C:\\Users\\jaywi\\Desktop\\launcherTest"
 
 # Load packages.json into memory
 
@@ -11,3 +15,10 @@ packages = json.loads(temp.read().decode('utf-8').replace("\n", ""))
 temp.close()
 del temp
 
+# Logic
+for mod in packages['mods']:
+    ident = compare.compare("{}".format(mod['hash']).format(baseUrl), baseModFolder + "\\" + mod['filename'])
+    if ident == True:
+        pass
+    else:
+        print("Doesnt match")
